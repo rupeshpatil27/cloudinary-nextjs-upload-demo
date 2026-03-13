bab99e20fba4039deda1447448bac82bf32caf4f
rupeshpatil <rupeshpatil88927@gmail.com>
2026-03-13 23:57:20 +0530

    implement robust file upload system with Cloudinary

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import UploadForm from "@/components/upload-form";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Cloudinary</h1>
-    </div>
+    <>
+      <UploadForm />
+    </>
   );
 }
